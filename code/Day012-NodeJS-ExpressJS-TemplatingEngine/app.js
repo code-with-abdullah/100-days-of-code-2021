8ad7e1887c151112e4753124d461c4ad02c266f5
Abdullah Aslam <shabdullahaslam@gmail.com>
2020-09-26 23:53:18 +0500

    Day 012 of #100DaysOfCode - templating engine - pug

---
+++ code/Day012-NodeJS-ExpressJS-TemplatingEngine/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'pug');  // setting PUG as the templating engine
+app.set('views', 'views');      // setting the file where views are saved
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
