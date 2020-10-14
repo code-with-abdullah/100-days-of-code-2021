3a761affe2275b74249e816cc826e61212286c8f
Abdullah Aslam <shabdullahaslam@gmail.com>
2020-10-14 23:40:22 +0500

    Implemented SQL database functionality

--- code/Day026-DatabaseMySQL/controllers/shop.js
+++ code/Day026-DatabaseMySQL/controllers/shop.js
@@ -2,25 +2,28 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(({rows, fieldData}) =>{
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
-  });
+  })
+  .catch(err => {console.log(err);});
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product=>{
-    res.render('shop/product-detail', 
-    {
-      product: product,
+  Product.findById(prodId)
+  .then(([product]) => {
+    res.render('shop/product-detail', {
+      product: product[0],
       pageTitle: product.title,
       path:'/product'
     });
-  });
+  })
+  .catch(err=>{console.log(err);});
 }
 
 exports.getIndex = (req, res, next) => {
--- code/Day026-DatabaseMySQL/models/product.js
+++ code/Day026-DatabaseMySQL/models/product.js
@@ -1,23 +1,6 @@
-const fs = require('fs');
-const path = require('path');
+cosnt db = require('../util/database');
 
 const cart = require('cart');
-
-const p = path.join(
-  path.dirname(require.main.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -29,49 +12,23 @@ module.exports = class Product {
   }
 
   save() {
-    
-    getProductsFromFile(products => {
-
-      if(this.id) {
-        const existingProductIndex = Products.findIndex(prod => prod.id === this.id);
-        const updatedProducts = [...Products];
-        updatedProducts[existingProductIndex] = this;
-
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random().toString();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
-  }
-
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+    return db.execute(
+      'INSERT INTO products {title, price, imageUrl, description} VALUES {?, ?, ?, ?}',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
   static deleteById(id) {
-
-    const product = products.find(prod => prod.id === id);
-
-    getProductsFromFile(products =>{
-      const updatedProducts = products.filter(prod => prod.id!==id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if(!err) {
-          Cart.deleteById(id);
-        }
-      });
-    });
   }
 
-  static findById(id, cb){
-    getProductsFromFile(products =>{
-      const product = products.find(p => p.id===id);
-      cb(product);
-    });
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
+  }
+
+  static findById(id){
+    return db.execute(
+      'SELECT * FROM products WHERE product.id = ?',
+      [id]
+    );
   }
 };
