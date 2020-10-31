46d26d993c23a67e1d8aa58da7b5c56ad07b9449
Abdullah Aslam <shabdullahaslam@gmail.com>
2020-10-31 23:46:52 +0500

    More functionality of Dashboard

--- code/Day032-InternsDotPk-ReactProject/start-learning-react/src/components/dashboard.js
+++ code/Day032-InternsDotPk-ReactProject/start-learning-react/src/components/dashboard.js
@@ -6,6 +6,8 @@ import WidgetText from './widgetText'
 import WidgetBar from './widgetBar';
 import WidgetDoughnut from './widgetDoughnut';
 
+import Dropdown from 'react-dropdown'
+
 import ReactFC from "react-fusioncharts";
 import FusionCharts from "fusioncharts";
 import Column2D from "fusioncharts/fusioncharts.charts";
@@ -25,7 +27,7 @@ const chartData = [
     {
     label: "Canada",
     value: "180"
-    }
+    } 
     // {
     // label: "Iran",
     // value: "140"
@@ -36,8 +38,40 @@ const chartData = [
     // }
 ];
 
+const config = {
+    apiKey: 'AIzaSyDMu-Vw30ykPPmFT3cXeunzKEi4EahzglI',
+    spreadsheetId: '1vcDPrMexD8bxNwwzK9IxF8wch6Hfezq2eooJACDiqgg'
+}
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetId
+    }/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${config.apiKey}`;
+
+
 
 class Dashboard extends Component {
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+        fetch(url)
+            .then(response=>response.json())
+            .then(data=>{
+                let batchRowValues = data.valueRanges[0].values;
+
+                const rows = [];
+
+                for (let i = 1; i < batchRowValues.length; i++) {
+                    let rowObject = {};
+                    for (let j = 0; j < batchRowValues[i].length; j++) {
+                        rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
+                    }
+                    rows.push(rowObject);
+                }
+            })
+    }
+
+
     render() {
         return (
             <div>
