d9ad6d2c34470d21283ecfebce28ee30a4a9340c
Abdullah Aslam <shabdullahaslam@gmail.com>
2020-10-31 23:37:28 +0500

    Adding widgets to Project Dashboard

--- code/Day032-InternsDotPk-ReactProject/start-learning-react/src/components/dashboard.js
+++ code/Day032-InternsDotPk-ReactProject/start-learning-react/src/components/dashboard.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+
 import './dashboard.css'
+
 import WidgetText from './widgetText'
+import WidgetBar from './widgetBar';
+import WidgetDoughnut from './widgetDoughnut';
+
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+
+const chartData = [
+    {
+    label: "Venezuela",
+    value: "290"
+    },
+    {
+    label: "Saudi",
+    value: "260"
+    },
+    {
+    label: "Canada",
+    value: "180"
+    }
+    // {
+    // label: "Iran",
+    // value: "140"
+    // },
+    // {
+    // label: "Russia",
+    // value: "115"
+    // }
+];
+
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
 
-                <WidgetText title="Title" value={120} description="Lorem ipsum dolor siet" />
+                {/* <WidgetText title="Title" value={120} description="Lorem ipsum dolor siet" />
+                <WidgetText title="Title 2" value={150} description="Lorem ipsum dolor siet amet" /> */}
+
+                {/* <WidgetBar title="title" data={chartData} /> */}
+
+                <WidgetDoughnut title="title" data={chartData} />
 
             </div>
         );
---
+++ code/Day032-InternsDotPk-ReactProject/start-learning-react/src/components/widgetDoughnut.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+function widgetDoughnut(props) {
+
+    const chartConfigs = {
+        type: "doughnut2d", // The chart type
+        width: "100%", // Width of the chart
+        height: "125", // Height of the chart
+        dataFormat: "json", // Data type
+        dataSource: {
+        // Chart Configuration
+        chart: {
+            bgColor: "#2a2a2a",
+            theme: "fusion"                 //Set the theme for your chart
+        },
+        // Chart Data - from step 2
+        data: props.data
+        }
+    };
+    
+
+    return (
+        <div>
+
+            <div className="widgetWrap">
+                <div className="widgetTitle">
+                    {props.title}
+                </div>
+
+                <div className="widgetValue">
+
+                    <ReactFC {...chartConfigs} />
+
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default widgetDoughnut;
