b670f746cf14712361c1cdec3dc530c8599c2bbe
Abdullah Aslam <shabdullahaslam@gmail.com>
2020-11-25 21:45:13 +0500

    Day 045 of #100DaysOfCode - Decision Tree and Random Forest Classification

---
+++ code/Day045-RandomForestClassification/code.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+print("Libraries imported")
+
+dataset = pd.read_csv('Data/Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, -1].values
+
+print("Data loaded")
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=50, criterion='entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+print("Model created")
+
+y_pred = classifier.predict(X_test)
+
+print("Predictions made")
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+print(cm)
